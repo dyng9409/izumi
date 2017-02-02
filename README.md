3568de08950b39c1bf9ce15430c53315a342a0c3
Dylan Nguyen <dyng9409@colorado.edu>
2017-02-02 01:49:32 -0700

    added mecab and parsing command started

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Izumi
 
 This is a bot designed for my bungo discord server
+
+Make token.txt with the bot token in project root directory
--- izumi.js
+++ izumi.js
@@ -2,15 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const path = require('path');
+const MeCab = require('mecab-async');
+const utf8 = require('utf8');
+var mecab = new MeCab();
+
+mecab.command = 'mecab -d dic';
 
 client.on('ready', () => {
     console.log('Izumi starting up!');
-});
-
-client.on('message', message => {
-    if(message.content === '!hello') {
-        message.channel.sendMessage('Hi! I am Izumi!');
-    }
 });
 
 fs.readFile(path.join(__dirname, 'token.txt'), function(err, token) {
@@ -20,4 +19,47 @@ fs.readFile(path.join(__dirname, 'token.txt'), function(err, token) {
     }
     client.login(token.toString().trim());
 });
+
+
+
+client.on('message', message => {
+    var msg = message.content.split(' ');
+    var command = msg[0];
+    var args = msg[1];
+
+    if ( command === '!hello') {
+        message.channel.sendMessage('Hi! I am Izumi!');
+    }
+    else if (command === '!echo') {
+        if ( args ) message.channel.sendMessage(args);
+        else message.channel.sendMessage('Nothing to echo');
+    }
+    else if (command === '!help') {
+        message.channel.sendMessage('Not yet implemented. Hang tight!');
+    }
+    else if (command === '!parse') {
+        if ( args ) {
+            console.log( args );
+            console.log( utf8.encode( args ) );
+            mecab.parse( 'うつりにけりな' , function (err, res) {
+                if (err) throw err;
+                console.log( res );
+                message.channel.sendMessage( res );
+            });
+        }
+        else message.channel.sendMessage('Nothing to parse!');
+    }
+    else if (command[0] === '!') {
+        message.channel.sendMessage('Unable to recognize command');
+        message.channel.sendMessage('Type !help for a list of available commands');
+    }
+});
+
+function parse( text ) {
+    mecab.parse( text.trim(), function(err, res) {
+        if (err) throw err;
+        console.log( res );
+        return res;
+    })
+    }
 
